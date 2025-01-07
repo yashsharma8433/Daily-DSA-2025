e38bb08e30a395ac76e54b7a55b2ebac164a1553
yash sharma <yashsharma843347@gmail.com>
2025-01-07 16:48:59 +0530

    Update file 07/01/2025, 16:48:59

--- 9-PalindromeNumber/9-PalindromeNumber.java
+++ 9-PalindromeNumber/9-PalindromeNumber.java
@@ -1,22 +1,20 @@
 class Solution {
     public boolean isPalindrome(int x) {
         int original = x;
-        int rev = 0 ; 
+        int rev = 0;
 
-        while (x > 0){
-            int ld = x%10 ; //121 then 1 aega 
-            rev = (rev*10)+ld; // 1 then 12 then 121
-            x = x / 10 ;
-
-
+        // Reverse the number
+        while (x > 0) {
+            int ld = x % 10; // Get the last digit
+            rev = (rev * 10) + ld; // Build the reversed number
+            x = x / 10; // Remove the last digit
         }
-if (original == rev){
- return true ;
-}
-else {
-    return false;
-}
 
-        
+        // Explicitly use an if statement
+        if (original == rev) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
